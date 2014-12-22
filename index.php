ef788d871fb02e62e293b40b86d5b725c7d245b2
Trinmar Boado <timi.boado@yahoo.com>
2014-12-22 15:06:03 +0800

    Update index.php
    the code
    error_reporting(E_ALL & ~E_NOTICE);
    will let us NOT to overwrite the PHP.ini and it will hide all the Error Notices

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL & ~E_NOTICE);
+
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
